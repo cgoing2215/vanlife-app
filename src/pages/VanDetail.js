8da46e1b45756d29ea67d11c430880d0bf5a901c
Caroline Going <carolinegoing2@gmail.com>
2023-11-03 10:44:54 -0400

    Added VanDetail nested page, pulling data from the selected van using params

--- src/pages/VanDetail.js
+++ src/pages/VanDetail.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import VansData from '../VansData'
+import { useParams } from 'react-router-dom';
 
-function VanDetail() {
-  return (
-    <div>VanDetail</div>
-  )
-}
 
-export default VanDetail
+const VanDetail = () => {
+    const { id } = useParams(); 
+
+    // Find the van based on the id
+    const van = VansData.find(van => {
+        return van.id === id;
+    });
+
+    return (
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} alt="van" />
+                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
+    );
+};
+
+export default VanDetail;
+
+
