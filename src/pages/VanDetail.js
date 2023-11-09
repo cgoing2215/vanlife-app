3e36b6f18e5065bd6461ac8390fc29ccc9a7d436
Caroline Going <carolinegoing2@gmail.com>
2023-11-08 19:33:33 -0500

    Removed loading message

--- src/pages/VanDetail.js
+++ src/pages/VanDetail.js
@@ -5,24 +5,20 @@ import { useParams } from 'react-router-dom';
 
 const VanDetail = () => {
     const { id } = useParams(); 
-
-    // Find the van based on the id
     const van = VansData.find(van => {
         return van.id === id;
     });
 
     return (
         <div className="van-detail-container">
-            {van ? (
-                <div className="van-detail">
-                    <img src={van.imageUrl} alt="van" />
-                    <i className={`van-type ${van.type} selected`}>{van.type}</i>
-                    <h2>{van.name}</h2>
-                    <p className="van-price"><span>${van.price}</span>/day</p>
-                    <p>{van.description}</p>
-                    <button className="link-button">Rent this van</button>
-                </div>
-            ) : <h2>Loading...</h2>}
+            <div className="van-detail">
+                <img src={van.imageUrl} alt="van" />
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+                <h2>{van.name}</h2>
+                <p className="van-price"><span>${van.price}</span>/day</p>
+                <p>{van.description}</p>
+                <button className="link-button">Rent this van</button>
+            </div>
         </div>
     );
 };
