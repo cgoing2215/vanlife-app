3eaf5339af8770b0cd412465643c8dc2f5150ee9
Caroline Going <carolinegoing2@gmail.com>
2023-11-29 16:19:23 -0500

    Created and styled footer, included below outlet within layout component

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <h1 className='footer-header'>Ⓒ 2022 #VANLIFE</h1>
+    </div>
+  )
+}
+
+export default Footer
